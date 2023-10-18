087ad34bd86a21b23e34359fcc5b6a91236d180b
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-10-18 13:04:08 +0200

    initial commit

---
+++ rent-car/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { SideBar } from "../SideBar/SideBar";
+
+const App = () => {
+  return (
+    <div>
+      <SideBar />
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ rent-car/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,20 @@
+import { nanoid } from "nanoid";
+export const SideBar = () => {
+  const inputModelId = nanoid();
+  const inputPriceId = nanoid();
+  const inputMileageId = nanoid();
+
+  return (
+    <aside>
+      <form>
+        <label htmlFor={inputModelId}>Car brand</label>
+        <input id={inputModelId} type="text"></input>
+        <label htmlFor={inputPriceId}>Price/ 1 hour</label>
+        <input id={inputPriceId} type="text"></input>
+        <label htmlFor={inputMileageId}>Сar mileage / km</label>
+        <input id={inputMileageId} type="text"></input>
+        <input id={inputMileageId} type="text"></input>
+      </form>
+    </aside>
+  );
+};
